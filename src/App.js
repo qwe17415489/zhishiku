453fe607ce4902135f4fec82ce7f4a43801cdab9
lilongfei <17415489@qq.com>
2023-11-02 17:52:32 +0800

    feat:调试自动构建

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          hello no word!
+          hello no word! 搞起来
         </a>
       </header>
     </div>
