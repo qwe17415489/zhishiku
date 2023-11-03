774cdf482f36322b8f899330b3bf9c1d732a171a
AKAking23 <82486465+AKAking23@users.noreply.github.com>
2023-11-03 11:24:41 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          hello no word! 搞起来啊
+          hello no word! 搞起来啊 奥利给
         </a>
       </header>
     </div>
